d6b82581b255d50737c8ba6c0788ae016fbc9fcb
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2022-11-24 00:07:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return "This Saturday, I want to " + activity + "!";
+}
+
+function mondayWork(activity="go to the office"){
+    return "This Monday, I will " + activity + "."; 
+}
+
+
+function wrapAdjective(symbol){
+    const innerFunction = (sentence) =>{
+        return `You are ${symbol}${sentence}${symbol}!`
+    }
+    return innerFunction
+}
+
+console.log(wrapAdjective())
+ 
